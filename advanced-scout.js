092e2a12afb4fb8e105b22e3f7d2d5747ccc66e0
2397 Robotics <2397robotics@gmail.com>
2025-04-08 13:57:06 -0700

    Update advanced-scout.js

--- advanced-scout.js
+++ advanced-scout.js
@@ -1,15 +1,29 @@
 // JavaScript for draggable functionality
 const draggables = document.querySelectorAll('.draggable');
+const scoutingFrame = document.querySelector('.scouting-frame');
 
 draggables.forEach(draggable => {
     draggable.addEventListener('mousedown', (e) => {
-        const rect = draggable.getBoundingClientRect();
-        const offsetX = e.clientX - rect.left;
-        const offsetY = e.clientY - rect.top;
+        let offsetX = e.clientX - draggable.getBoundingClientRect().left;
+        let offsetY = e.clientY - draggable.getBoundingClientRect().top;
 
         function onMouseMove(event) {
-            draggable.style.left = `${event.clientX - offsetX}px`;
-            draggable.style.top = `${event.clientY - offsetY}px`;
+            // Calculate new position
+            let newX = event.clientX - offsetX;
+            let newY = event.clientY - offsetY;
+
+            // Constrain movement within the scouting frame
+            const frameRect = scoutingFrame.getBoundingClientRect();
+            const draggableRect = draggable.getBoundingClientRect();
+
+            if (newX < frameRect.left) newX = frameRect.left;
+            if (newY < frameRect.top) newY = frameRect.top;
+            if (newX + draggableRect.width > frameRect.right) newX = frameRect.right - draggableRect.width;
+            if (newY + draggableRect.height > frameRect.bottom) newY = frameRect.bottom - draggableRect.height;
+
+            // Apply new position
+            draggable.style.left = `${newX - frameRect.left}px`;
+            draggable.style.top = `${newY - frameRect.top}px`;
         }
 
         function onMouseUp() {
